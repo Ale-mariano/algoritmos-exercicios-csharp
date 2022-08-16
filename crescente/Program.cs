3ae4791bc10c68e4cb1e75e16694370be1ea9e8d
Ale-mariano <alemariano29@gmail.com>
2022-08-16 15:07:29 -0300

    exercicio csharp

---
+++ crescente/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace menor_de_tres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x, y;
+
+            Console.Write("Digite dois numeros: ");
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            while (x != y){
+                if (x < y){
+                    Console.Write("CRESCENTE!");
+                 }
+             else {
+                Console.Write("DECRESCENTE!");
+            }
+
+            Console.Write("Digite outros dois numeros: ");
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+
+
+        }
+    }
+}
